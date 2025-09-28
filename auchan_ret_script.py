81f1d53c0f7ae28157c7ca6a304a4e2ae090d75c
zarov.ilya <mrk_dev@i-media.ru>
2025-09-28 21:13:41 +0300

    Основной скрипт работает. Осталось завернуть в ООП, прописать сохранение и ссылку на фид. И мэйн добавить2

---
+++ auchan_ret_script.py
@@ -0,0 +1,74 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+pd.set_option('display.width', 1500)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', 100)
+# pd.set_option('display.max_rows', None)
+
+class RemarketingFeedMatch:
+    """Класс для поиска соответствий в фиде для офферного ремаркетинга"""
+
+tree = ET.parse("moscow.xml")
+root = tree.getroot()
+
+def feed_to_dataframe(rows_limit: int = None) -> pd.DataFrame:
+    """Преобразует фид в DataFrame"""
+    data = []
+
+    for i, v in enumerate(root.iter('offer')):
+        if rows_limit is not None and i >= rows_limit:
+            break
+
+        offer_data = {
+            'id': v.attrib.get('id'),
+            'name': v.findtext('name'),
+            'url': v.findtext('url'),
+            'price': float(v.findtext('price')),
+            'categoryId': v.findtext('categoryId'),
+        }
+
+        data.append(offer_data)
+
+    return pd.DataFrame(data)
+
+def filter_has_matches(df):
+    """Фильтрует только те строки, для которых есть альтернативное значение"""
+    df['first_3'] = df['name'].apply(lambda x: ' '.join(x.split()[:3]))
+    df['matches_count'] = df['first_3'].apply(lambda x: df['first_3'].to_list().count(x))
+    df = df[df['matches_count'] > 1]
+
+    return df
+
+mass = feed_to_dataframe()
+mass = filter_has_matches(mass)
+
+def pair(element: pd.Series):
+    try:
+        filtered_mass = mass[
+            (mass['price'] > element['price']) &
+            (mass['price'] < element['price']*1.2) &
+            (mass['first_3'] == element['first_3'])
+        ].sort_values(by=['price']).reset_index(drop=True)
+
+        if not filtered_mass.empty:
+            # Если есть совпадения
+            a = filtered_mass.iloc[0]
+            element['new_id'] = a['id']
+            element['new_name'] = a['name']
+            element['new_url'] = a['url']
+            element['new_price'] = a['price']
+        else:
+            # Если нет совпадений
+            element['new_id'] = None
+            element['new_name'] = None
+            element['new_url'] = None
+            element['new_price'] = None
+
+    except Exception as e:
+        print(f"Error: {str(e)}. Element: {element}")
+
+    return element
+
+mass = mass.apply(pair, axis=1)
+print(mass)
