92b3a3c0128d61d37d95c5a8de3559a103bf5f29
zarov.ilya <mrk_dev@i-media.ru>
2025-10-01 20:06:47 +0300

    CHN-629 Fixed functions off the class

--- feed_retargeting_finder/auchan_ret_script.py
+++ feed_retargeting_finder/auchan_ret_script.py
@@ -1,8 +1,9 @@
+from datetime import datetime
+from pathlib import Path
 import xml.etree.ElementTree as ET
+
 import pandas as pd
 import requests
-from pathlib import Path
-from datetime import datetime
 
 pd.set_option('display.width', 1500)
 pd.set_option('display.max_columns', None)
@@ -15,23 +16,10 @@ class RemarketingFeedMatch:
     def __init__(self, feed):
         tree = requests.get(feed)
         self.root = ET.fromstring(tree.content)
+        self.df = None
 
-    def file_save(self, df: pd.DataFrame, mode: int = 0):
-        """Защищенный метод. Создает путь к файлу в указанной папке."""
-        if mode == 0:
-            # Сохранить в файл
-            folder_path = Path(__file__).parent / 'outputs'
-            folder_path.mkdir(parents=True, exist_ok=True)
-
-            file_path = folder_path / f'remarketing_find_{datetime.now().strftime("%Y%m%d-%H%M%S")}.xlsx'
-
-            df.to_excel(file_path, index=False)
-        elif mode == 1:
-            # Вывести в консоль
-            print(df)
-
-    def feed_to_dataframe(self, rows_limit: int = None) -> pd.DataFrame:
-        """Преобразует фид в DataFrame"""
+    def feed_to_dataframe(self, rows_limit: int = None):
+        """Преобразовать фид в DataFrame"""
         data = []
 
         for i, v in enumerate(self.root.iter('offer')):
@@ -48,23 +36,26 @@ class RemarketingFeedMatch:
 
             data.append(offer_data)
 
-        return pd.DataFrame(data)
+        self.df = pd.DataFrame(data)
 
-    def filter_has_matches(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Фильтрует только те строки, для которых есть альтернативное значение"""
+    def filter_has_matches(self):
+        """Отфильтровать только те строки, для которых есть альтернативное значение"""
+        df = self.df
         df['first_3'] = df['name'].apply(lambda x: ' '.join(x.split()[:3]))
         df['matches_count'] = df['first_3'].apply(lambda x: df['first_3'].to_list().count(x))
         df = df[df['matches_count'] > 1]
 
-        return df
+        self.df = df
 
-    def pair(self, element: pd.Series, mass: pd.DataFrame):
+    def pair(self, element):
+        """Подставить альтернативное значение для строки в DataFrame"""
+        mass = self.df
         try:
             filtered_mass = mass[
                 (mass['price'] > element['price']) &
-                (mass['price'] < element['price']*1.2) &
+                (mass['price'] < element['price'] * 1.2) &
                 (mass['first_3'] == element['first_3'])
-            ].sort_values(by=['price']).reset_index(drop=True)
+                ].sort_values(by=['price']).reset_index(drop=True)
 
             if not filtered_mass.empty:
                 # Если есть совпадения
@@ -85,17 +76,40 @@ class RemarketingFeedMatch:
 
         return element
 
+    def apply_pair(self):
+        """Применить pair ко всем строкам"""
+        df = self.df
+        df = df.apply(lambda x: self.pair(x), axis=1)
+        self.df = df
 
+    def final_view(self):
+        """Убрать вспомогательные столбцы и отфильтровать непустые альт. значения"""
+        df = self.df
+        df = df.drop(columns=['first_3', 'matches_count'])
+        df = df.mass[df['new_name'].notna()]
+        self.df = df
+
+    def file_save(self, mode: int = 0):
+        """Создает путь к файлу в указанной папке."""
+        df = self.df
+        if mode == 0:
+            # Сохранить в файл
+            folder_path = Path(__file__).parent / 'outputs'
+            folder_path.mkdir(parents=True, exist_ok=True)
+            file_path = folder_path / f'remarketing_find_{datetime.now().strftime("%Y%m%d-%H%M%S")}.xlsx'
+            df.to_excel(file_path, index=False)
+        elif mode == 1:
+            # Вывести в консоль
+            print(df)
 
 def main():
     feed = 'https://skrypnikovmk.com/moscow.xml'
     ex = RemarketingFeedMatch(feed)
-    mass = ex.feed_to_dataframe(50)  # 50 это для теста. Для всего фида - убираем аргумент
-    mass = ex.filter_has_matches(mass)
-    mass = mass.apply(lambda x: ex.pair(x, mass), axis=1)
-    mass = mass.drop(columns=['first_3', 'matches_count'])
-    mass = mass[mass['new_name'].notna()]
-    ex.file_save(mass, 1)  # 0 - вывести в файл, 1 - в консоль
+    ex.feed_to_dataframe(50)
+    ex.filter_has_matches()
+    ex.apply_pair()
+    ex.final_view()
+    ex.file_save(1)
 
 if __name__ == '__main__':
     main()
